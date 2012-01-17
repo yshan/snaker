d2bbeaa43e4d76e376f0262b0f5c52a0bdd297f0
yinshan1980@gmail.com <yinshan1980@gmail.com@f5a2ec27-6349-93de-7a48-f57d792a5771>
2012-01-17 06:28:38 +0000

    set the default charset as utf8

--- Snaker/startSnaker.bat
+++ Snaker/startSnaker.bat
@@ -1,1 +1,1 @@
-start "Snaker" java -Xss4m -Xms512m -Xmx512m -cp snaker.jar;lib/* com.snaker.Entry
+start "Snaker" java -Dfile.encoding=UTF8 -Xss4m -Xms512m -Xmx512m -cp snaker.jar;lib/* com.snaker.Entry
--- Snaker/startSnaker.sh
+++ Snaker/startSnaker.sh
@@ -3,4 +3,4 @@
 WORK_DIR=`dirname $0`
 cd $WORK_DIR
 
-java -Xss4m -Xms512m -Xmx512m -cp snaker.jar:lib/* com.snaker.Entry
+java -Dfile.encoding=UTF8 -Xss4m -Xms512m -Xmx512m -cp snaker.jar:lib/* com.snaker.Entry
